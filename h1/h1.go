c3f56921631ec61935741be1ebc0b0ae52ec7178
Luke Young <bored-engineer@users.noreply.github.com>
2019-02-17 11:16:34 -0800

    Fix typos in helper method comments

--- h1/h1.go
+++ h1/h1.go
@@ -6,10 +6,10 @@ const GraphQLEndpoint = "https://hackerone.com/graphql"
 // Bool allocates a new bool value to store v at and returns a pointer to it.
 func Bool(v bool) *bool { return &v }
 
-// String allocates a new bool value to store v at and returns a pointer to it.
+// String allocates a new string value to store v at and returns a pointer to it.
 func String(v string) *string { return &v }
 
-// Int allocates a new bool value to store v at and returns a pointer to it.
+// Int allocates a new int32 value to store v at and returns a pointer to it.
 func Int(v int32) *int32 { return &v }
 
 // Float allocates a new float64 value to store v at and returns a pointer to it.
